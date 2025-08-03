e62b3795e4e1f0f7d4655773406afc22e15def03
Harris Kaufmann <st4ck0v3rfl0w@gmail.com>
2025-08-03 15:12:50 +0200

    better testing

--- x/src/main.rs
+++ x/src/main.rs
@@ -2,54 +2,61 @@ use chacha20poly1305::{aead::Aead, aead::AeadInOut, legacy::ChaCha20Poly1305Lega
 use sodiumoxide::crypto::aead::chacha20poly1305::{self as sodiumoxidecipher, Key, Nonce, Tag};
 
 fn main() {
-    let mykey = b"qweruiodskgdfvbskdjhfvbksjdhfbvk";
-    let mynonce = b"ihuoruiw";
-    let myplain = b"01hello world!23";
-    let myad = b"blub";
+    let key = b"qweruiodskgdfvbskdjhfvbksjdhfbvk";
+    let nonce = b"ihuoruiw";
+    let msg = b"01hello world!23";
+    let ad = b"blub";
 
-    let mut rustbuf = myplain.to_owned();
-    let rust = ChaCha20Poly1305Legacy::new(mykey.into());
+    roundtrip_rc_to_sodium(*key, *nonce, msg, ad);
+    roundtrip_sodium_to_rc(*key, *nonce, msg, ad);
+}
+
+fn roundtrip_rc_to_sodium(key: [u8; 32], nonce: [u8; 8], msg: &[u8], ad: &[u8]) {
+    let mut rustbuf = msg.to_owned();
+    let rust = ChaCha20Poly1305Legacy::new(&key.into());
     let rusttag = rust
-        .encrypt_inout_detached(
-            mynonce.into(),
-            myad.as_slice(),
-            rustbuf.as_mut_slice().into(),
-        )
+        .encrypt_inout_detached(&nonce.into(), ad, rustbuf.as_mut_slice().into())
         .unwrap();
 
-    let rustcipherhex = hex::encode(rustbuf);
-    let rusttaghex = hex::encode(rusttag);
+    let rustcipherhex = hex::encode(&rustbuf);
+    let rusttaghex = hex::encode(&rusttag);
 
     println!("{rustcipherhex}{rusttaghex}");
 
-    let mut sodiumbuf = myplain.to_owned();
+    sodiumoxidecipher::open_detached(
+        &mut rustbuf,
+        Some(ad),
+        &Tag::from_slice(&rusttag).unwrap(),
+        &Nonce::from_slice(&nonce).unwrap(),
+        &Key::from_slice(&key).unwrap(),
+    )
+    .unwrap();
+
+    assert!(rustbuf == msg);
+
+    let rustplainstring = core::str::from_utf8(&rustbuf).unwrap();
+    println!("{rustplainstring}");
+}
+
+fn roundtrip_sodium_to_rc(key: [u8; 32], nonce: [u8; 8], msg: &[u8], ad: &[u8]) {
+    let mut sodiumbuf = msg.to_owned();
 
     let sodiumtag = sodiumoxidecipher::seal_detached(
         &mut sodiumbuf,
-        Some(myad),
-        &Nonce::from_slice(mynonce).unwrap(),
-        &Key::from_slice(mykey).unwrap(),
+        Some(ad),
+        &Nonce::from_slice(&nonce).unwrap(),
+        &Key::from_slice(&key).unwrap(),
     );
 
-    let sodiumcipherhex = hex::encode(sodiumbuf);
-    let sodiumtaghex = hex::encode(sodiumtag);
+    let sodiumcipherhex = hex::encode(&sodiumbuf);
+    let sodiumtaghex = hex::encode(&sodiumtag);
 
     println!("{sodiumcipherhex}{sodiumtaghex}");
 
-    sodiumoxidecipher::open_detached(
-        &mut rustbuf,
-        Some(myad),
-        &Tag::from_slice(&rusttag).unwrap(),
-        &Nonce::from_slice(mynonce).unwrap(),
-        &Key::from_slice(mykey).unwrap(),
-    )
-    .unwrap();
-    let rustplainstring = core::str::from_utf8(&rustbuf).unwrap();
-    println!("{rustplainstring}");
-
+    let rust = ChaCha20Poly1305Legacy::new(&key.into());
     rust.decrypt_inout_detached(
-        mynonce.into(),
-        myad.as_slice(),
+        &nonce.into(),
+        ad,
         sodiumbuf.as_mut_slice().into(),
         &sodiumtag.0.into(),
     )
