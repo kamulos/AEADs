c5456fb5d292c5309429bb84bcd4cd6c4f6731a6
Harris Kaufmann <st4ck0v3rfl0w@gmail.com>
2025-08-03 15:28:40 +0200

    better testing

--- x/src/main.rs
+++ x/src/main.rs
@@ -1,14 +1,18 @@
-use chacha20poly1305::{aead::Aead, aead::AeadInOut, legacy::ChaCha20Poly1305Legacy, KeyInit};
+use chacha20poly1305::{aead::AeadInOut, legacy::ChaCha20Poly1305Legacy, KeyInit};
 use sodiumoxide::crypto::aead::chacha20poly1305::{self as sodiumoxidecipher, Key, Nonce, Tag};
 
 fn main() {
-    let key = b"qweruiodskgdfvbskdjhfvbksjdhfbvk";
-    let nonce = b"ihuoruiw";
-    let msg = b"01hello world!23";
-    let ad = b"blub";
+    for _ in 0..5000 {
+        let key = rand::random();
+        let nonce = rand::random();
+        let msg_size = rand::random_range(50..2000);
+        let msg = rand::random_iter().take(msg_size).collect::<Vec<u8>>();
+        let ad_size = rand::random_range(50..2000);
+        let ad = rand::random_iter().take(ad_size).collect::<Vec<u8>>();
 
-    roundtrip_rc_to_sodium(*key, *nonce, msg, ad);
-    roundtrip_sodium_to_rc(*key, *nonce, msg, ad);
+        roundtrip_rc_to_sodium(key, nonce, &msg, &ad);
+        roundtrip_sodium_to_rc(key, nonce, &msg, &ad);
+    }
 }
 
 fn roundtrip_rc_to_sodium(key: [u8; 32], nonce: [u8; 8], msg: &[u8], ad: &[u8]) {
@@ -18,10 +22,10 @@ fn roundtrip_rc_to_sodium(key: [u8; 32], nonce: [u8; 8], msg: &[u8], ad: &[u8]) 
         .encrypt_inout_detached(&nonce.into(), ad, rustbuf.as_mut_slice().into())
         .unwrap();
 
-    let rustcipherhex = hex::encode(&rustbuf);
-    let rusttaghex = hex::encode(&rusttag);
+    // let rustcipherhex = hex::encode(&rustbuf);
+    // let rusttaghex = hex::encode(&rusttag);
 
-    println!("{rustcipherhex}{rusttaghex}");
+    // println!("{rustcipherhex}{rusttaghex}");
 
     sodiumoxidecipher::open_detached(
         &mut rustbuf,
@@ -34,8 +38,8 @@ fn roundtrip_rc_to_sodium(key: [u8; 32], nonce: [u8; 8], msg: &[u8], ad: &[u8]) 
 
     assert!(rustbuf == msg);
 
-    let rustplainstring = core::str::from_utf8(&rustbuf).unwrap();
-    println!("{rustplainstring}");
+    // let rustplainstring = core::str::from_utf8(&rustbuf).unwrap();
+    // println!("{rustplainstring}");
 }
 
 fn roundtrip_sodium_to_rc(key: [u8; 32], nonce: [u8; 8], msg: &[u8], ad: &[u8]) {
@@ -48,10 +52,10 @@ fn roundtrip_sodium_to_rc(key: [u8; 32], nonce: [u8; 8], msg: &[u8], ad: &[u8]) 
         &Key::from_slice(&key).unwrap(),
     );
 
-    let sodiumcipherhex = hex::encode(&sodiumbuf);
-    let sodiumtaghex = hex::encode(&sodiumtag);
+    // let sodiumcipherhex = hex::encode(&sodiumbuf);
+    // let sodiumtaghex = hex::encode(&sodiumtag);
 
-    println!("{sodiumcipherhex}{sodiumtaghex}");
+    // println!("{sodiumcipherhex}{sodiumtaghex}");
 
     let rust = ChaCha20Poly1305Legacy::new(&key.into());
     rust.decrypt_inout_detached(
@@ -62,6 +66,6 @@ fn roundtrip_sodium_to_rc(key: [u8; 32], nonce: [u8; 8], msg: &[u8], ad: &[u8]) 
     )
     .unwrap();
 
-    let sodiumplainstring = core::str::from_utf8(&sodiumbuf).unwrap();
-    println!("{sodiumplainstring}");
+    // let sodiumplainstring = core::str::from_utf8(&sodiumbuf).unwrap();
+    // println!("{sodiumplainstring}");
 }
